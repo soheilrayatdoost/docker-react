87c01557f2e2fbf9fe1b8f92050c5b3b89b610f5
Soheil Rayatdoost <srayatdo@lab4tech.lan>
2023-08-09 17:15:51 +0200

    added text

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Salam Mehrnaz! Dooset daram :*
+          Salam Mehrnaz! Dooset daram :* ghashang
         </p>
         <a
           className="App-link"
