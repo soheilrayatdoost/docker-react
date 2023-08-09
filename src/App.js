4830d846cc31c53939ff3c434011cd6ae0ecf8e3
Soheil Rayatdoost <srayatdo@lab4tech.lan>
2023-08-09 14:09:16 +0200

    changed the text of web

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hi there!
+          Salam Mehrnaz! Dooset daran :*
         </p>
         <a
           className="App-link"
